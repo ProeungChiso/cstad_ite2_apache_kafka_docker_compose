e8d0328ac51b231c328c7b02123f2b9de3cf13bd
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2024-09-06 18:33:19 +0700

    config

---
+++ push-code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add all changes to the staging area
+git add .
+
+# Prompt for commit message
+read -p "Enter commit message: " commit_message
+
+# Commit changes
+git commit -m "$commit_message"
+
+# Push changes to the remote repository
+git push
